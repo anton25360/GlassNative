05a8d87f2e7f3427bed5c0ef128c08eb92ae4717
Anton Webb <anton25360@gmail.com>
2020-09-08 16:28:42 +0100

    removed font-weight property

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -66,7 +66,7 @@ function TabTwoNavigator() {
       <TabTwoStack.Screen
         name="TabTwoScreen"
         component={TabTwoScreen}
-        options={{ headerTitle: 'Favourites' , headerTitleStyle:{fontFamily:'productSans-bold', fontWeight:200}}}
+        options={{ headerTitle: 'Favourites' , headerTitleStyle:{fontFamily:'productSans-bold'}}}
       />
     </TabTwoStack.Navigator>
   );
