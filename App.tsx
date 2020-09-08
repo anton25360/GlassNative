23f36868c8eea6d520ae831db77faf7531b56498
Anton Webb <anton25360@gmail.com>
2020-09-08 15:49:28 +0100

    Added Product sans font

--- App.tsx
+++ App.tsx
@@ -1,17 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as Font from 'expo-font'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+//load product sans font
+const getFonts = () => Font.loadAsync({
+  'productSans-regular': require('./assets/fonts/ProductSans-Regular.ttf')
+})
+
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [fontsLoaded, setFontsLoaded] = useState(false)
 
   if (!isLoadingComplete) {
-    return null;
+    getFonts()
+    return (
+      null
+    )
   } else {
     return (
       <SafeAreaProvider>
