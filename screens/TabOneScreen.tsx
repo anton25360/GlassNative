ea87eea09e5127aaa3200879dcfd7ec6927352ab
Anton Webb <anton25360@gmail.com>
2020-09-08 14:16:37 +0100

    Removed searchBar component, implemented directly onto parent

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,23 +1,68 @@
-import React, {useState, Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
+import { SearchBar } from 'react-native-elements';
 
-import SearchBar from '../components/SearchBar'
+// import SearchBar from '../components/SearchBar'
 import SearchResults from '../components/SearchResults'
-// import SearchScreen from '../components/SearchScreen'
 
+
+
+
+//start render
 export default class TabOneScreen extends Component {
- 
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search:string) => {
+    this.setState({ search });
+    console.log(search);
+    // this.props.onUserSearch(search);
+    // this.props.name('hello')
+
+  };
+
   render() {
+    const { search } = this.state;
+
     return (
       <View style={styles.container}>
-        <SearchBar ></SearchBar>
-        <Text>Hello!2</Text>
+        {/* <SearchBar getSearch={printThis} ></SearchBar> */}
+
+
+        <SearchBar
+          placeholder='eg: Mojito'
+          onChangeText={this.updateSearch}
+          value={search}
+        />
+
+
+
+    <Text>{search}</Text>
         <SearchResults></SearchResults>
       </View>
     )
+  }
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
