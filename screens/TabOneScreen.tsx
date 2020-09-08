566cc86f657a61bfac850d84bd732b13944ef8d1
Anton Webb <anton25360@gmail.com>
2020-09-08 11:04:15 +0100

    Implemented SearchBar component

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+// import { SearchBar } from 'react-native-elements';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import SearchBar from '../components/SearchBar'
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <SearchBar></SearchBar>
       <Text>Hello!</Text>
     </View>
   );
@@ -14,17 +17,12 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    // flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center'
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  }
 });
