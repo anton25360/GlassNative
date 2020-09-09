24a5c2000d1de039ce5a573e62fd1aef336b150d
Anton Webb <anton25360@gmail.com>
2020-09-09 12:10:15 +0100

    Implemented cocktailDB api

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -6,46 +6,69 @@ import { SearchBar } from 'react-native-elements';
 //custom components
 import SearchResults from '../components/SearchResults'
 
+//get cocktail data
+let getData = (input: string) => {
+  console.log('calling api...');
+  console.log(input);
+
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + input)
+    .then(function (data) {
+      return data.json()
+    })
+    .then(function (data){
+      var result = data.drinks[0] //gets 1st result
+      console.log(result);
+      
+      
+    })
+
+
+
+
+    }
+
 //start render
 export default class TabOneScreen extends Component {
-  state = {
-    search: '',
-  };
+    state = {
+      search: '',
+    };
 
-  updateSearch = (search: string) => {
-    this.setState({ search });
+    updateSearch = (search: string) => {
+      this.setState({ search });
+      getData(search)
 
-  };
+    };
 
-  render() {
-    const { search } = this.state;
+    render() {
+      const { search } = this.state;
 
-    return (
-      <View style={styles.container}>
+      return (
+        <View style={styles.container}>
 
-        <SearchBar
-          // containerStyle={{backgroundColor:'transparent', }}
-          placeholder='eg: Mojito'
-          onChangeText={this.updateSearch}
-          value={search}
+          <SearchBar
+            // containerStyle={{backgroundColor:'transparent', }}
+            placeholder='eg: Mojito'
+            onChangeText={this.updateSearch}
+            value={search}
           // lightTheme={true}
-        />
+          />
 
-        <Text style={styles.title}>{search}</Text>
-        <SearchResults></SearchResults>
-      </View>
-    )
+          <Text style={styles.title}>{search}</Text>
+          <SearchResults></SearchResults>
+        </View>
+      )
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderColor: 'red',
-    // borderWidth: 2
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      borderColor: 'red',
+      // borderWidth: 2
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      // fontFamily: 'productSans-regular',
+    }
+  });
