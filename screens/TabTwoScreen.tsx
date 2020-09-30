7575984d8bb66904aab6eaf76ed65ad183e30c48
Anton Webb <anton25360@gmail.com>
2020-09-30 16:37:12 +0100

    removed comments

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -32,29 +32,23 @@ export default class TabTwoScreen extends Component {
       .then((value) => this.setState({ favsArray: value }))
       .catch((err) => console.log("AsyncStorageErr: " + err));
 
-    // let favourites = this.state.favsArray;
-    // <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-    // this.setState({favsArray:})
-
     // sets selected drink in state and opens the modal
     let showModal = (drinkNameObject: string) => {
-      // this.state.currentDrink = Object.values(drinkNameObject)[0];
       this.setState({ currentDrink: Object.values(drinkNameObject)[0] });
-
-      // this.state.showModal = true;
       this.setState({ showModal: true });
     };
 
     //cloes the modal
     let closeModal = () => {
-      // this.state.showModal = false;
       this.setState({ showModal: false });
     };
 
+    //returns the favourites in custom component
     const items = this.state.favsArray.map(function (item) {
       return <FavouriteItem name={item} preview={showModal} />;
     });
 
+    //decides wether or not the 'you have no favs, message is shown
     let hasFavourites = false;
     if (this.state.favsArray.length == 0) {
       hasFavourites = false;
@@ -85,36 +79,12 @@ export default class TabTwoScreen extends Component {
           {items}
         </ScrollView>
 
-        {/* <View>
-          <Modal
-            isVisible={this.state.showModal}
-            // backdropTransitionOutTiming={0}
-            hideModalContentWhileAnimating={true}
-            useNativeDriver={false}
-            // animationIn={"slideInUp"}
-            // animationOut={"slideOutUp"}
-          >
-            <View style={{ flex: 1 }}>
-              <Text>{this.state.currentDrink}</Text>
-
-              <TouchableOpacity
-                style={styles.closeModalBtn}
-                onPress={() => {
-                  closeModal();
-                }}
-              >
-                <Text style={styles.closeModalBtnText}>Remove</Text>
-              </TouchableOpacity>
-            </View>
-          </Modal>
-        </View> */}
-
         <Modal
           animationType="slide"
           transparent={true}
           visible={this.state.showModal}
           onRequestClose={() => {
-            alert("Modal has been closed.");
+            console.log("Modal has been closed.");
           }}
         >
           <View style={styles.modalShadow}>
@@ -141,7 +111,6 @@ const styles = StyleSheet.create({
   message: {
     fontSize: 17,
     fontFamily: "productSans-regular",
-    // color:'grey',
   },
   container: {
     flex: 1,
@@ -152,29 +121,20 @@ const styles = StyleSheet.create({
 
   modalShadow: {
     backgroundColor: "rgba(52, 52, 52, 0.5)",
-
     flex: 1,
   },
   modalContent: {
-    // borderColor: "red",
-    // borderWidth: 2,
     flex: 1,
     marginHorizontal: "10%",
     marginVertical: "30%",
-    // backgroundColor: "blue",
-    // textAlign: 'center'
-    // justifyContent:'center'
     alignItems: 'center',
     borderRadius:3
   },
   closeModalBtn: {
     backgroundColor: "#ff3d54",
-    // flexGrow: 1,
     padding: 10,
     paddingHorizontal:40,
     borderRadius: 3,
-    margin: 10,
-    marginLeft: 5,
   },
   closeModalBtnText: {
     fontFamily: "productSans-regular",
